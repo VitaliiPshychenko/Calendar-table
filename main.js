6ccec807e8ea69d725f6f6beab0e2c5169339f03
Vitalii Pshychenko <pshychenko94@gmail.com>
2019-05-16 22:01:18 +0300

    update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function createCalendar(id, year, month) {
   const container = document.getElementById(id);
-  const date = new Date(year, month - 1);
+  const currentMonth = month - 1;
+  const date = new Date(year, currentMonth);
   const days = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Нд'];
   let table = '<table><tr>';;
 
@@ -9,19 +10,11 @@ function createCalendar(id, year, month) {
   }
   table += '</tr><tr>';
 
-  function getDay(weekDay) {
-    let day = weekDay.getDay();
-    if (day === 0) {
-      day = 7;
-    }
-    return day - 1;
-  }
-
   for (let i = 0; i < getDay(date); i++) {
     table += '<td></td>';
   }
 
-  while (date.getMonth() === month - 1) {
+  while (date.getMonth() === currentMonth) {
     table += `<td>${date.getDate()}</td>`;
     if (getDay(date) % 7 === 6) {
       table += '</tr><tr>';
@@ -37,6 +30,14 @@ function createCalendar(id, year, month) {
   table += '</tr></table>';
  
   container.innerHTML = table;
+
+  function getDay(weekDay) {
+    let day = weekDay.getDay();
+    if (day === 0) {
+      day = 7;
+    }
+    return day - 1;
+  }
 }
 
 createCalendar("cal", 2012, 9);
